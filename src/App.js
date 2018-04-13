9af96487659d5e7690ab52600dc1df7b1ff2c884
Katie Lentes <katie.lentes@homechef.com>
2018-04-13 15:08:15 -0500

    Components: Libray completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
-import Albums from './components/Album';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -19,7 +19,8 @@ class App extends Component {
        <main>
          <Route exact path="/" component={Landing} />
          <Route path="/library" component={Library} />
-         <Route path="/album" component={Albums} />
+         <Route exact path="/" component={Album} />
+         <Route path="/album/:slug" component={Album} />
         </main>
       </div>
     );
